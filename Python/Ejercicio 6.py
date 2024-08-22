d35d7c42115d30ec67dde7878f4db838983770e2
matt <danmsuarez111@soy.sena.edu.co>
2024-08-22 15:10:43 -0500

    Algoritmo desarrollado

--- Python/Ejercicio 6.py
+++ Python/Ejercicio 6.py
@@ -1,11 +1,12 @@
 import random
 
-lista_almuerzo = []
+def  almuerzos_semana(n):
+     almuerzo = random.sample(n,5)
+     return print(almuerzo)
+    
+lista_almuerzo = [input(f"{i+1}. Ingresa almuerzo: ")for i in range(10)]
+
+    
+almuerzos_semana(lista_almuerzo)
 
 
-for i in range(10):
-  ingreso = input("Ingresa almuerzo")
-  lista_almuerzo.append(ingreso)
-
-
-print(lista_almuerzo)
